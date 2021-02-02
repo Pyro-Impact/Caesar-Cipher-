bca419c6613403dba176dd4b7d5c737b768dc72e
Pyro-Impact <77907654+Pyro-Impact@users.noreply.github.com>
2021-02-02 07:56:36 -0800

    Updated to be more Refined.
    Also subject to change in the future.

--- Caesar Cipher.py
+++ Caesar Cipher.py
@@ -1,28 +1,31 @@
 print("Welcome to the Caesar Cipher Encryptor/Decryptor")
-print("Please type only letters, numbers, and spaces.")
+print("Please type only letters, numbers, spaces and commas/periods.")
 print("The Caesar Cipher shifts alphabetically forward 15 units. For example, a becomes P.")
 
 Encrypt = {"a": "P", "b": "Q", "c": "R", "d": "S", "e": "T", "f": "U", "g": "V", "h": "W", "i": "X", "j": "Y",
            "k": "Z", "l": "A", "m": "B", "n": "C", "o": "D", "p": "E", "q": "F", "r": "G", "s": "H", "t": "I",
-           "u": "J", "v": "K", "w": "L", "x": "M", "y": "N", "z": "O", " ": " ", "1": "1", "2": "2", "3": "3",
-           "4": "4", "5": "5", "6": "6", "7": "7", "8": "8", "9": "9", "0": "0"}
+           "u": "J", "v": "K", "w": "L", "x": "M", "y": "N", "z": "O", "A": "p", "B": "q", "C": "r", "D": "s",
+           "E": "t", "F": "u", "G": "v", "H": "w", "I": "x", "J": "y", "K": "z", "L": "a", "M": "b", "N": "c",
+           "O": "d", "P": "e", "Q": "f", "R": "g", "S": "h", "T": "i", "U": "j", "V": "k", "W": "l", "X": "m",
+           "Y": "n", "Z": "o", " ": " ", "1": "4", "2": "5", "3": "6", "4": "7", "5": "8", "6": "9", "7": "0",
+           "8": "1", "9": "2", "0": "3", ".": ",", ",": "."}
 
 Decrypt = {v: k for k, v in Encrypt.items()}
 
 while True:
 
     while True:
-        answer = input("Would you like to encrypt or decrypt a message? Please type encrypt or decrypt.")
-        if answer.lower() in ["encrypt", "decrypt"]:
+        answer = input("Would you like to encrypt or decrypt a message? Please type encrypt or decrypt: ")
+        if answer.strip() in ["encrypt", "decrypt"]:
             break
         print("Invalid input. Please try again.")
 
     while True:
-        if answer.lower() == "encrypt":
-            message = str(input("Please enter the code you'd like to encrypt"))
+        if answer.strip() in "encrypt":
+            message = str(input("Please enter the code you'd like to encrypt: "))
             encrypt_for = ""
-            for i in message.lower():
-                encrypt_for += message.lower()
+            for i in message:
+                encrypt_for += message
                 if i not in Encrypt:
                     print("Invalid input. Please try again.")
                 else:
@@ -30,10 +33,10 @@ while True:
             if True:
                 break
         else:
-            message = input("Please enter the code you'd like to decrypt")
+            message = input("Please enter the code you'd like to decrypt: ")
             decrypt_for = ""
-            for t in message.upper():
-                decrypt_for += message.upper()
+            for t in message:
+                decrypt_for += message
                 if t not in Decrypt:
                     print("Invalid input. Please try again.")
                 else:
@@ -44,26 +47,26 @@ while True:
     def cryptocode():
         result = ""
         if answer.lower() == "encrypt":
-            for e in message.lower():
+            for e in message:
                 result += Encrypt[e]
         elif answer.lower() == "decrypt":
-            for r in message.upper():
+            for r in message:
                 result += Decrypt[r]
         return result
 
     print("The message translates into:", cryptocode())
 
     while True:
-        restart_confirm = input("Would you like to encrypt/decrypt another message?"
-                                "Type yes or no")
+        restart_confirm = input("Would you like to encrypt/decrypt another message? \n"
+                                "Type yes or no: ")
 
-        if restart_confirm.lower() in ["yes", "no"]:
+        if restart_confirm.strip() in ["yes", "no"]:
             break
         else:
-            print("Invalid input. Type yes or no.")
+            print("Invalid input. Please type yes or no.")
             continue
 
-    if restart_confirm.lower() == "yes":
+    if "yes" in restart_confirm.lower():
         print("The program has reset.")
         continue
     else:
