57fadc9445aac7bd5de1668b6c71a8c711a27f01
Pyro-Impact <77907654+Pyro-Impact@users.noreply.github.com>
2021-02-05 09:56:43 -0800

    removed unecessary file

--- Encrypt_or_Decrypt.py
+++
@@ -1,8 +0,0 @@
-def en_or_de():
-    while True:
-        answer = input("Would you like to encrypt or decrypt a message? Please type encrypt or decrypt: ")
-        answer1 = answer.strip().lower()
-        if answer1 in ["encrypt", "decrypt"]:
-            break
-        print("Invalid input. Please try again.")
-    return answer1
